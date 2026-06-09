7c9962c3e42a8b7029eb1954680acf22cb2becc4
Harsh saini <harshsainib@gmail.com>
2026-06-09 21:37:09 +0530

    Fix CORS configuration to include origin

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
 app.use(cors( {
-  origin: process.env.ORIGIN 
+  origin: process.env.ORIGIN ,
   credentials: true
 }));
 app.use(express.json());
