78287209d36a9abf600f0180beef9a65bd028201
Harsh saini <harshsainib@gmail.com>
2026-06-10 12:02:38 +0530

    Enhance Loading component with message prop
    Add optional message prop to Loading component.

--- frontend/src/component/Loading.jsx
+++ frontend/src/component/Loading.jsx
@@ -1,12 +1,17 @@
 import { Helix } from "ldrs/react";
 import "ldrs/react/Helix.css";
 
-// Default values shown
-
-export const Loading = () => {
+export const Loading = ({ message = false }) => {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="flex h-screen w-full flex-col items-center justify-center">
       <Helix size="89" speed="2.5" color="black" />
+      
+      {/* Conditionally render the text if message is true */}
+      {message && (
+        <p className="mt-4 text-center text-sm text-gray-500">
+          This may take a few minutes. Please wait...
+        </p>
+      )}
     </div>
   );
 };
